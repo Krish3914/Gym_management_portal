d3cdda8cc802f7d79a9453914711ba993dec845f
ShubhamNikam07 <shubhamnikamsn007@gmail.com>
2023-12-21 19:09:05 +0530

    small signup update

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -19,24 +19,3 @@ const dbConnect = ()=> {
 module.exports = dbConnect;
 
 
-
-// const mongoose = require("mongoose");
-// const dbURL = "mongodb+srv://shubhamnikamsn007:R5YF6gMe4D6KLXn2@cluster0.v5qwzji.mongodb.net/shubhamDB";
-// const dbURL2 = "mongodb://localhost:27017/auth";
-// require("dotenv").config();
-
-// const dbConnect =()=>{
-//     mongoose.connect(dbURL2,{
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true        
-//     })
-//     .then(()=>{
-//         console.log("db connection is successfull");
-//     })
-//     .catch(()=>{
-//         console.log("unable to connect with db please try later");
-//         process.exit(1);
-//     })
-// }
-
-// module.exports = dbConnect;
