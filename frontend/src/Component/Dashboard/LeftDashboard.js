ee36115381577da13568085ca418c909ed162975
Karan Parge <151928417+karanparge@users.noreply.github.com>
2024-04-11 13:04:50 +0530

    Add files via upload

--- frontend/src/Component/Dashboard/LeftDashboard.js
+++ frontend/src/Component/Dashboard/LeftDashboard.js
@@ -1,4 +1,9 @@
 import { FaChevronRight } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { FaTable } from "react-icons/fa";
+import { FaHeadset } from "react-icons/fa";
+import { FaUserPlus } from "react-icons/fa";
+import { FaQuestionCircle } from "react-icons/fa";
 import iconImage from "../../images/favicon/favicon.ico";
 import { Link } from "react-router-dom";
 import { Searchbar } from "../Searchbar";
@@ -15,36 +20,35 @@ export const LeftDashboard = () => {
               <img src={logopng}/>
             </div>
           </Link>
-          <div className="bg-purple-200 p-3 font-medium opacity-80 rounded-md text-purple-700">
-            <Link to={"navbar"}> Dashboard</Link>
+          <div className="bg-indigo-100 text-blue-700 p-3 font-medium opacity-80 rounded-md flex align-items:center gap-3">
+            <Link to={"navbar"} className="hover:text-gray-600 display: inline-flex">
+              <FaHome style={{ fontSize: "18px", marginRight: "15px", marginLeft:"5px", marginBottom:"5px", marginTop:"5px"}}/><div className="flex align-items:center" style={{ marginBottom:"5px", marginTop:"5px"}}>Dashboard</div> 
+            </Link>
           </div>
-          <div className="bg-purple-200 p-3 font-medium opacity-80 rounded-md flex justify-between flex-col gap-3">
-            <Link to={"/dashboard/traineeform"}>
+
+          <div className="text-grey-400 p-3 font-medium opacity-80 rounded-md flex justify-between flex-col gap-3">
+          <Link to={"/dashboard/traineeform"} className="hover:text-gray-600 display: inline-flex">
+              <FaUserPlus style={{ fontSize: "18px", marginRight: "15px", marginLeft:"5px", marginBottom:"5px", marginTop:"5px"}}/><div className="flex align-items:center" style={{ marginBottom:"5px", marginTop:"5px"}}>Add User</div> 
+            </Link>
+          </div>
+          <div className="text-grey-400 p-3 font-medium opacity-80 rounded-md flex align-items:center gap-3">
+            <Link to={"/dashboard/tables"}className="hover:text-gray-600">
               <div className="flex justify-between items-center cursor-pointer adduser">
-                Add User
+              <FaTable style={{ fontSize: "18px", marginRight: "15px", marginLeft:"5px", marginBottom:"5px", marginTop:"5px" }}/><div className="flex align-items:center" style={{ marginBottom:"5px", marginTop:"5px"}}>Table</div>
               </div>
             </Link>
           </div>
-          <div className="bg-purple-200 p-3 font-medium opacity-80 rounded-md flex justify-between flex-col gap-3">
-            <Link to={"/dashboard/tables"}>
-              <div className="flex justify-between items-center cursor-pointer adduser">
-                Table
-              </div>
-            </Link>
+          <div className="text-grey-400 p-3 font-medium opacity-80 rounded-md flex justify-between flex-col gap-3">
+          <Link to={"/dashboard/support"} className="hover:text-gray-600 display: inline-flex items-center"> 
+            <FaHeadset style={{ fontSize: "18px", marginRight: "15px", marginLeft:"5px", marginBottom:"5px", marginTop:"5px"}} />  
+            <div className="flex align-items:center" style={{ marginBottom:"5px", marginTop:"5px"}}>Support</div>
+          </Link>
           </div>
-          <div className="bg-purple-200 p-3 font-medium opacity-80 rounded-md flex justify-between flex-col gap-3">
-            <Link to={"/dashboard/support"}>
-              <div className="flex justify-between items-center cursor-pointer adduser">
-                Support
-              </div>
-            </Link>
-          </div>
-          <div className="bg-purple-200 p-3 font-medium opacity-80 rounded-md flex justify-between flex-col gap-3">
-            <Link to={"/dashboard/FAQ"}>
-              <div className="flex justify-between items-center cursor-pointer adduser">
-                FAQ
-              </div>
-            </Link>
+          <div className="text-grey-400 p-3 font-medium opacity-80 rounded-md flex justify-between flex-col gap-3">
+          <Link to={"/dashboard/FAQ"} className="hover:text-gray-600 display: inline-flex items-center"> 
+            <FaQuestionCircle style={{ fontSize: "18px", marginRight: "15px", marginLeft:"5px", marginBottom:"5px", marginTop:"5px"}} />  
+            <div className="flex align-items:center" style={{ marginBottom:"5px", marginTop:"5px"}}>FAQ</div>
+          </Link>
           </div>
         </div>
       </div>
